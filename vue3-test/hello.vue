3fd443b9b0b626f6a7214cc882a8b37c0bbbe81b
zhangxingyu618 <1846730600@qq.com>
2021-06-01 09:27:22 +0800

    第一次提交

---
+++ vue3-test/hello.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+<div v-for="item in items" :key="item.id">
+    {{ item }}
+</div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
